0c62e53bf1fd0acce05f827559058ea53f747b71
SSupattapone <samuelsupattapone@hanovernorwichschools.org>
2015-09-24 14:27:12 -0400

    Updated from Brython Server: 9/24/2015 2:27:18 PM

--- birthday.py
+++ birthday.py
@@ -35,6 +35,6 @@ name = input("Hello, what is your name? ")
 month = input("Hi {0}, what was the name of the month you were born in? " .format(name))
 year = input("And what year were you born in, {0}? " .format(name))
 day = input("And the day? ")
-if month = October and day = 31
-print("You were born on Halloween!")
+if month == "October" and day == 31:
+    print("You were born on Halloween!")
 print("{0}, you are a  " .format(name))
